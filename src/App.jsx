3408c35648b1a08ec473e3edcd6667480a93ea74
Luiz Santos <luiz.santos@cgvadvogados.com.br>
2023-09-21 09:46:38 -0300

    Ajuste css e jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className="card">
+      <div className="card" tag="adicional">
         <h2>Rick Sanchez</h2>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
